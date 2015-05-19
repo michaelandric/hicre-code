a76d7677397a6019301d0424ced6bf01b9271753
Michael Andric <andric.michael@gmail.com>
2015-05-19 17:09:18 +0200

    mapnames_ancillary
    takes regions (148) and map values to suma MNI brain

---
+++ mapnames_ancillary.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 12:35:30 2015
+
+@author: andric
+
+
+How I got the surface node labels from the anot file:
+
+ConvertDset -i lh.aparc.a2009s.annot.niml.dset -o_1Dp \
+-prepend_node_index_1D -prefix lh_node_labels
+ConvertDset -i rh.aparc.a2009s.annot.niml.dset -o_1Dp \
+-prepend_node_index_1D -prefix rh_node_labels
+"""
+
+import time
+import os
+import pandas as pd
+import numpy as np
+
+suma_dir = '/Applications/AFNI/suma_MNI_N27/'
+ct = pd.read_csv('CTallregionsBOTH.csv')
+ctnames = pd.Series(ct.columns[3:151], name='region_name')
+
+# see above for how I made these 1D labels files
+lh_labels = np.loadtxt('lh_node_labels.1D.dset')
+rh_labels = np.loadtxt('rh_node_labels.1D.dset')
+
+# this is same as "lh.aparc.a2009s.annot.1D.cmap" and
+# "rh.aparc.a2009s.annot.1D.cmap"
+rois = open('rois.aparc.cmap', 'r').readlines()
+
+nm_rows = np.array(rois)[np.arange(1719, 1869, 2)]
+idents = np.array(map(int,
+                      [rw.split()[5].split('(')[1].split(')')[0]
+                          for rw in nm_rows]))
+idents_f = np.delete(idents, np.where(np.in1d(idents, 1644825)))
+lh_idents_dict = dict(zip(np.array(idents_f), ctnames[0:74]))
+rh_idents_dict = dict(zip(np.array(idents_f), ctnames[74:148]))
+
+blindLV = open('blind.pls2LVs.txt').readlines()
+lv1 = []
+lv2 = []
+for b in blindLV:
+    lv1.append(b.split()[1])
+    lv2.append(b.split()[2])
+
+lv1 = np.array(lv1, dtype=np.float64)
+lv2 = np.array(lv2, dtype=np.float64)
+
+lv_table = np.column_stack([ctnames, lv1, lv2])
+lv_dict = dict(zip(ctnames, zip(lv1, lv2)))
+
+for h in ['lh', 'rh']:
+    print '%s -- ' % h+time.ctime()
+    roi_aparc = '%s.aparc.a2009s.annot.1D.roi' % h
+    aparc = np.loadtxt(os.path.join(suma_dir, roi_aparc))
+    ncols = 2
+    surf_out = np.array(np.zeros(len(aparc)*ncols)).reshape(len(aparc), ncols)
+    if h == 'lh':
+        lbls = lh_labels
+        indents_dict = lh_idents_dict
+    elif h == 'rh':
+        lbls = rh_labels
+        indents_dict = rh_idents_dict
+
+    for i in xrange(len(lbls[:, 1])):
+        try:
+            region = indents_dict[int(lbls[i, 1])]
+            surf_out[i, :] = lv_dict[region]
+        except:
+            surf_out[i, :] = np.zeros(ncols)
+    labels_out = '%s_lv_labels.1D' % h
+    np.savetxt(labels_out, np.column_stack((aparc[:, 0], surf_out)),
+               fmt='%i %f %f')
